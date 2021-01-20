6d34dbdfd6b9ddb4adc330fadb3f64d61a253870
ankurkasliwal185 <ankurkasliwal185@gmail.com>
2021-01-20 23:28:11 +0530

    added test cases

--- src/test/java/stringcalc/StringCalculatorTest.java
+++ src/test/java/stringcalc/StringCalculatorTest.java
@@ -6,12 +6,56 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-    public static void main(String args[]) {
-      org.junit.runner.JUnitCore.main("stringcalc.StringCalculatorTest");
+    private StringCalculator calculator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void initialize() {
+        calculator = new StringCalculator();
     }
+
+//     public static void main(String args[]) {
+//       org.junit.runner.JUnitCore.main("stringcalc.StringCalculatorTest");
+//     }
     
     @Test
-  	public void testEmptyString() {
-		assertEquals(0, Calculator.add(""));
-	  }
+    public void testEmptyString() {
+	assertEquals(0, calculator.add(""));
+    }
+	
+    @Test
+    public void testOneNumber() {
+	assertEquals(1, calculator.add("1"));
+	assertEquals(calculator.add("5"), 5);
+    }
+  
+    @Test
+    public void testTwoNumbers(){
+        assertEquals(3, calculator.add("1,2"));
+    }
+    @Test
+    public void testThreeNumbers(){
+    	assertEquals(6, calculator.add("1,2,3"));
+    }
+	
+    @Test
+    public void testNewLine(){
+    	assertEquals(6, calculator.add("1\n2,3"));
+    }
+	
+    @Test
+    public void threeNumbersDelimitedAnywayShouldBeSummed() {
+        assertEquals(calculator.add("1,2,3"), 6);
+        assertEquals(calculator.add("5\n2\n3"), 10);
+    }
+	
+    @Test
+    public void negativeInputReturnsException() {
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Negative input!");
+        calculator.add("-1");
+        calculator.add("-5,10\n-15");
+    }	
 }
